87cf2e52afd31fd493059bd98b689e9c24923aeb
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-07-26 10:29:08 +0530

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import { signOut } from 'firebase/auth'
 import React from 'react'
 import { auth } from '../firebase'
+import { useContext } from 'react'
+import { AuthContext } from '../Context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser}=useContext(AuthContext)
   return (
     <div className='flex items-center bg-[#041C32] h-[50px] p-[10px] justify-between text-white'>
       <span className="font-bold">Welcome</span>
       <div className="flex gap-[10px]">
-        <img src="" alt="" className='h-[24px] w-[24px] rounded-[50%] object-cover' />
-        <span>Himanshu</span>
+        <img src={currentUser.photoURL} alt="" className='h-[24px] w-[24px] rounded-[50%] object-cover' />
+        <span>{currentUser.displayName}</span>
         <button className='bg-[#04293A] border-none text-white cursor-pointer font-bold text-[10px] px-2 rounded-sm'onClick={()=>signOut(auth)}>Logout</button>
       </div>
     </div>
