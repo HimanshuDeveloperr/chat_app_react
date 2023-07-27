bf5de7ae72cbf827dcec7d76f423987eb0bb9712
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-07-27 14:48:23 +0530

    search users functionality completed ***

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "./../firebase";
 
 const Search = () => {
-  return (
-    <div className='border-b-[1px] border-[#EEEEEE]'>
-      <div className="p-[10px]">
-        <input type="text" className='bg-transparent outline-none text-white placeholder:text-[lightgrey]' placeholder='find a user'/>
-      </div>
-      <div className="p-[10px] flex items-center gap-[10px] text-white cursor-pointer hover:bg-[#041C32]">
-        <img src="" alt="" className='w-[50px] h-[50px] rounded-[50%] object-cover'/>
-        <div className="">
-          <span className='font-[500] text-[18px]'>Himanshu</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
 
-export default Search
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  return (
+    <div className="border-b-[1px] border-[#EEEEEE]">
+      <div className="p-[10px]">
+        <input
+          type="text"
+          className="bg-transparent outline-none text-white placeholder:text-[lightgrey]"
+          placeholder="find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </div>
+      {err && <span className="text-[red]">user not found</span>}
+      {user && (
+        <div className="p-[10px] flex items-center gap-[10px] text-white cursor-pointer hover:bg-[#041C32]">
+          <img
+            src={user.photoURL}
+            alt=""
+            className="w-[50px] h-[50px] rounded-[50%] object-cover"
+          />
+          <div className="">
+            <span className="font-[500] text-[18px]">{user.displayName}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
